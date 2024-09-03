d8979c2081ee89ebaea68cca4bd2f4fa75405523
Garvit Singhal <82756460+GarvitSinghal47@users.noreply.github.com>
2024-09-03 10:47:18 +0530

    Update test.js

--- netlify/function/test.js
+++ netlify/function/test.js
@@ -1,21 +1,25 @@
-const invalidEmailDomains = [
-  '0-mail.com',
-  '027168.com',
-  '0815.su',
-  '0sg.net',
-  '10mail.org',
-  '10minutemail.co.za',
-  '11mail.com',
-  '123.com'
-];
-
 exports.handler = async (event) => {
+  const invalidEmailDomains = [
+    '0-mail.com',
+    '027168.com',
+    '0815.su',
+    '0sg.net',
+    '10mail.org',
+    '10minutemail.co.za',
+    '11mail.com',
+    '123.com'
+  ];
+
   const { emailDomain } = JSON.parse(event.body);
   
   const isInvalid = invalidEmailDomains.includes(emailDomain);
 
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+      'Access-Control-Allow-Headers': 'Content-Type', // Allow specific headers
+    },
     body: JSON.stringify({ isInvalid }),
   };
 };
